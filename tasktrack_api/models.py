50f2de4e4d9e024505c25bc09520ea3e9369103a
Guille-Avila <guillollermo.95@gmail.com>
2023-05-16 00:46:01 -0500

    model creation

---
+++ tasktrack_api/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+# Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Enter a valid email")
+        if not password:
+            raise ValueError("Enter avalid password")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, max_length=150)
+    password = models.CharField(max_length=128)
+    username = models.CharField(max_length=150, unique=True)
+    name = models.CharField(max_length=150)
+    phone = models.CharField(max_length=20)
+    company = models.CharField(max_length=150)
+    college = models.CharField(max_length=150)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    due_date = models.DateField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    done_date = models.DateTimeField(null=True, blank=True)
+    done = models.BooleanField(default=False)
+    priority = models.ForeignKey('Priority', on_delete=models.CASCADE)
+    lista = models.ForeignKey('List', on_delete=models.CASCADE)
+    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    users = models.ManyToManyField(User)
+
+class Priority(models.Model):
+    name = models.CharField(max_length=100)
+
+class List(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
