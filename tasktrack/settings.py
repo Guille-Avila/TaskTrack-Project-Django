10a2be6609c989c693bb45019e0659a27b80d935
Guille-Avila <guillollermo.95@gmail.com>
2023-06-10 00:28:29 -0500

    runtime

--- tasktrack/settings.py
+++ tasktrack/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = "django-insecure-dbkjw-5j-1l1m$eudlasgv@y2o@wt_!ugii1sgup5yu&xwe)nl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost',
-                 'tasktrack-project-django-production.up.railway.app']
+# General domain to host app
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -135,10 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # python manage.py collectstatic
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
