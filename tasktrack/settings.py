2bf501f66d704ea2669cc7dc3cb0dd78e17e91f4
Guille-Avila <guillollermo.95@gmail.com>
2023-06-09 22:07:15 -0500

    config static

--- tasktrack/settings.py
+++ tasktrack/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-dbkjw-5j-1l1m$eudlasgv@y2o@wt_!ugii1sgup5yu&xwe)nl"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'tasktrack-project-django-production.up.railway.app']
 
@@ -133,7 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = 'C:/Users/Owner/OneDrive/Escritorio/projects/tasktrack/tasktrack-back/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
