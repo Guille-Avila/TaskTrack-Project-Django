b52d359f19b109ba2680a6381622371e65f4887d
Guille-Avila <guillollermo.95@gmail.com>
2023-06-10 22:57:07 -0500

    push x3

--- tasktrack/settings.py
+++ tasktrack/settings.py
@@ -53,10 +53,10 @@ INSTALLED_APPS = [
 # available request links cors
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
-    "https://magenta-marzipan-7ec353.netlify.app"
-]
+# CORS_ORIGIN_WHITELIST = [
+#     "http://localhost:3000",
+#     "https://magenta-marzipan-7ec353.netlify.app"
+# ]
 CORS_ALLOW_METHODS = [
     "GET",
     "POST",
