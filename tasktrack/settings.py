973d16e1945cd05793e6cb0c89b41a90bd4d9368
Guille-Avila <guillollermo.95@gmail.com>
2023-06-10 22:00:10 -0500

    middleware library

--- tasktrack/settings.py
+++ tasktrack/settings.py
@@ -52,17 +52,16 @@ INSTALLED_APPS = [
 ]
 
 # available request links cors
-# CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ALLOW_CREDENTIALS = True
-# CORS_ORIGIN_WHITELIST = [
-#     "http://localhost:3000",
-# ]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     # import corsheaders
-    "tasktrack_api.middleware.CorsMiddleware",
-    # "corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     # import white noise
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
